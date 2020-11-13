bcd2540afc2b9c71260a6ca1ba239868487d1cdf
FourtyNos <68012241+FourtyNos@users.noreply.github.com>
2020-11-13 13:53:48 +0100

    Update fast_install.sh

--- fast_install.sh
+++ fast_install.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+#############################################################
+##                      Fast_install.sh                    ##
+##                     Made by FourtyNos                   ##
+##   A Skript made for SSH Users. Its purpose is to make   ##
+##   it easier to handle the Pi via SSH and to have some   ##
+##   tools that can Help.                                  ##
+#############################################################
+
+
 # run with "mc" installs Midnight Commander and its easy to use for file management
 # Credit to: https://midnight-commander.org/
 sudo apt-get install mc -Y
