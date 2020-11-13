5e5e36ad619bdee06ac26be959898027c30cf4bd
FourtyNos <68012241+FourtyNos@users.noreply.github.com>
2020-11-13 13:48:37 +0100

    Update fast_install.sh

--- fast_install.sh
+++ fast_install.sh
@@ -1,2 +1,9 @@
-#bin.bash
+#!/bin/sh
+
+# run with "mc" installs Midnight Commander and its easy to use for file management
+# Credit to: https://midnight-commander.org/
+sudo apt-get install mc -Y
+
+
+
 
